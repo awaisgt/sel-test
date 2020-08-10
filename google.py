0cc28b024228e797208a58ff8b36a5fe4307e93a
awaisgt <39981782+awaisgt@users.noreply.github.com>
2020-08-11 01:32:26 +0500

    Update google.py

--- google.py
+++ google.py
@@ -1,10 +1,22 @@
-import os
+"""import os
 from selenium import webdriver
 op = webdriver.ChromeOptions()
 op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 op.add_argument("--headless")
 op.add_argument("--no-sandbox")
 op.add_argument("--disable-dev-sh-usage")
-driver = webdriver.Chrome(os.environ.get("CHROMEDRIVER_PATH"),op)
-driver.get("https://www.github.com")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=op)
+driver.get('https://google.com')
+print(driver.page_source)"""
+
+
+from selenium import webdriver
+GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
+CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.binary_location = GOOGLE_CHROME_PATH
+browser = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+driver.get('https://google.com')
 print(driver.page_source)
